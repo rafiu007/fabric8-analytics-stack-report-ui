82582cf1b6f24f79083283fceae7b31255bf9e49
Rafiu Jaman Mollah <rmollah@redhat.com>
2021-04-28 21:31:50 +0530

    Added component to use context data from api.

---
+++ src/app/components/shared-components/addons-primary/data.tsx
@@ -0,0 +1,15 @@
+import React, { useState,useContext } from 'react';
+import Context from '../../../store/context';
+import SelectableDataList from '../../shared-components/addons-primary/datalist';
+const Data = () => {
+    const data = useContext(Context);
+    return(
+        <div>
+            {data.map(apiData => (
+                <SelectableDataList name={apiData.recommendation.companion.name} score={apiData.recommendation.companion.coocurrence_probablity}>
+
+                </SelectableDataList>
+            ))}
+        </div>
+    );
+}
